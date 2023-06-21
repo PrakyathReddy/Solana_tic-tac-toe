69f3e0aed38d15e3e32e45fc8122674602caa999
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-21 19:12:51 +0530

    added game state, symbol, struct and enum. added depencies and  imported num_derive and num_traits

--- programs/tic-tac-toe-anchor/src/lib.rs
+++ programs/tic-tac-toe-anchor/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use num_derive::*;
+use num_traits::*;
 
 declare_id!("BwAT2NVQuxS4wuvzSd4MjPUbxMZm4yv791C7E62yYJUp");
 
@@ -13,3 +15,34 @@ pub mod tic_tac_toe_anchor {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
+#[account]
+pub struct Game {
+    players: [Pubkey; 2],
+    turn: u8,
+    board: [[Option<Sign>; 3]; 3], 
+    state: GameState,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum GameState {
+    Active,
+    Tie,
+    Won { winner: Pubkey },
+}
+
+#[derive(
+    AnchorSerialize,
+    AnchorDeserialize,
+    FromPrimitive,
+    ToPrimitive,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq
+)]
+pub enum Sign {
+    X,
+    O,
+}
